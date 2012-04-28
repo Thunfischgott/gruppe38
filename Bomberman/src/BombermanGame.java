270d111d70df0a37f64bee41a07d0157b7bd2699
Tom Berwald <tom.berwald@uni-duesseldorf.de>
2012-04-28 09:25:53 +0200

    Unsere main-Methode

---
+++ Bomberman/src/BombermanGame.java
@@ -0,0 +1,14 @@
+
+public class BombermanGame {
+
+	/**
+	 * @param args
+	 * 
+	 * Unsere schöne main :)
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
